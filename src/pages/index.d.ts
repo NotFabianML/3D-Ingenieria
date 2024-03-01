8e9d922f275040b408ee0c6c62b2d7ae559836d9
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-02-29 21:56:06 -0600

    update pages index.d.ts

--- src/pages/index.d.ts
+++ src/pages/index.d.ts
@@ -1,5 +1,5 @@
 import Home from '@/pages/Home'
-import About from '@/sections/About'
+import About from '@/pages/About'
 import Services from '@/pages/Services'
 import Projects from '@/pages/Projects'
 import Contact from '@/pages/Contact'
